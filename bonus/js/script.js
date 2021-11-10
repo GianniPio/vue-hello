774356a45af4fc92708f5a0b34e5e1f648146e58
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-10 16:51:57 +0100

    finished bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -3,7 +3,10 @@ var prova = new Vue (
     {
         el: '#container',
         data: {
-            message: 'Hello word'
+            message: 'Quando riesci a fare l esercizio',
+            immagine: 'https://www.notizie24h.it/wp-content/uploads/2021/10/meme_gif_3-10-21-5.gif',
+            miaClasse: "centrato",
+            intero: "full"
         }
 
     }
